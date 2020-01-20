f1a809c4a71c85b8b4a4b4ab1ecc7d2a2537e53a
Eszter Szluka <szluka.eszter@gmail.com>
2020-01-20 14:56:37 +0100

    'setup config and pom'

---
+++ service-checkout/src/main/java/com/codecool/servicecheckout/ServiceCheckoutApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.servicecheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCheckoutApplication.class, args);
+	}
+
+}
