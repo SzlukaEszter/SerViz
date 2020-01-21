bd120d4f8baad7c9bd4026392ce64d03a6eb4fb7
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-01-21 10:18:40 +0100

    add new water method

--- service-water/src/main/java/com/codecool/servicewater/ServiceWaterApplication.java
+++ service-water/src/main/java/com/codecool/servicewater/ServiceWaterApplication.java
@@ -14,6 +14,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 
 @SpringBootApplication
@@ -40,6 +41,7 @@ public class ServiceWaterApplication {
     }
 
     @Bean
+    @PostConstruct
     public CommandLineRunner init() {
 
         return args -> {
